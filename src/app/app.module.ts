f1dc2ac3a69b01ce94d7ef2804b4f9757837460e
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-09-09 20:36:21 +0200

    Added header and welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './Components/header/header.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent
   ],
     imports: [
         BrowserModule,
