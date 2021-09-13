1269729d3433a15acfdafcaa1ad27be0d1132adc
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-09-13 20:38:10 +0200

    Added click event to pictures

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  scooterText = "";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClickLime(){
+    this.scooterText = "Lime is your ideal carbon-free option for any trip, any time, anywhere.";
+  }
+  onClickLink(){
+    this.scooterText = "Link is your ideal carbon-free option for any trip, any time, anywhere.";
+  }
+  onClickTier(){
+    this.scooterText = "Tier is your ideal carbon-free option for any trip, any time, anywhere.";
+  }
+  onClickBird(){
+    this.scooterText = "Bird is your ideal carbon-free option for any trip, any time, anywhere.";
+  }
 }
