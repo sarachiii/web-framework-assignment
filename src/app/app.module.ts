410005c3df684a1e7136ea75bfbeb81a1ad21eed
Nazlıcan Eren <nazlican.eren@hva.nl>
2021-09-20 11:41:45 +0200

    Created 2 components: overview32 & detail32

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { Overview31Component } from './components/scooters/overview31/overview31.component';
+import { Overview32Component } from './components/scooters/overview32/overview32.component';
+import { Detail32Component } from './components/scooters/detail32/detail32.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { Overview31Component } from './components/scooters/overview31/overview31
     WelcomeComponent,
     NavBarComponent,
     Overview31Component,
+    Overview32Component,
+    Detail32Component,
 
   ],
     imports: [
---
+++ src/app/components/scooters/overview32/overview32.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview32',
+  templateUrl: './overview32.component.html',
+  styleUrls: ['./overview32.component.css']
+})
+export class Overview32Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
