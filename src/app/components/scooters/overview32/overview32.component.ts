64dec8049096529588b37c9ea5457eaa777f2663
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-09-20 13:38:17 +0200

    Working on Overview32 component

--- src/app/components/scooters/overview32/overview32.component.ts
+++ src/app/components/scooters/overview32/overview32.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Scooter} from "../../../models/scooter";
 
 @Component({
   selector: 'app-overview32',
   templateUrl: './overview32.component.html',
-  styleUrls: ['./overview32.component.css']
+  styleUrls: ['./overview32.component.css'],
 })
 export class Overview32Component implements OnInit {
 
   scooters: Scooter[];
   pId: number = 30000;
+  clicked: boolean = null;
+  // selected = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     this.scooters = [];
     for (let i = 0; i < 8; i++) {
       this.scooters.push(Scooter.createSampleScooter(this.pId));
       this.pId += 3; //increase with a (random) increment of about 3 for each new scooter?
-      console.log(this.scooters[i]);
     }
   }
 
   onAddScooter() {
     this.scooters.push(Scooter.createSampleScooter(this.pId));
     this.pId += 3; // increase with a (random) increment of about 3 for each new scooter?
+    // this.selected.push(true);
+    this.clicked = true;
+  }
+
+  selectedScooter(i) {
   }
 }
