d670170070648c36bc1195f5cb2fe08c159f430d
Nazlıcan Eren <nazlican.eren@hva.nl>
2021-09-20 12:00:11 +0200

    table overview32

--- src/app/components/scooters/overview32/overview32.component.ts
+++ src/app/components/scooters/overview32/overview32.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Scooter} from "../../../models/scooter";
 
 @Component({
   selector: 'app-overview32',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Overview32Component implements OnInit {
 
+  scooters: Scooter[];
+  pId: number = 30000;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.scooters = [];
+    for (let i = 0; i < 8; i++) {
+      this.scooters.push(Scooter.createSampleScooter(this.pId));
+      this.pId += 3; //increase with a (random) increment of about 3 for each new scooter?
+      console.log(this.scooters[i]);
+    }
   }
 
+  onAddScooter() {
+    this.scooters.push(Scooter.createSampleScooter(this.pId));
+    this.pId += 3; // increase with a (random) increment of about 3 for each new scooter?
+  }
 }
