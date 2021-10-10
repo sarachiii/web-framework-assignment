578ea90dd089395930df778fa120e3517fbc998c
Nazlıcan Eren <nazlican.eren@hva.nl>
2021-10-10 12:27:25 +0200

    3.4.1 changed

--- src/app/components/scooters/detail34/detail34.component.ts
+++ src/app/components/scooters/detail34/detail34.component.ts
@@ -8,78 +8,73 @@ import {Subscription} from "rxjs";
   selector: 'app-scooters-detail34',
   templateUrl: './detail34.component.html',
   styleUrls: ['./detail34.component.css'],
-  providers: [ScootersService]
 })
 export class Detail34Component implements OnInit {
-  @Input() public _selectedScooterFromOverview : Scooter;
-  @Output() selectedScooterEmitter = new EventEmitter<Scooter>();
-  @Output() deletedSelected = new EventEmitter<Scooter>();
-  @Output() cancelSelected = new EventEmitter<Scooter>();
-  @Output() resetSelected = new EventEmitter<Scooter>();
-  @Output() clearSelected = new EventEmitter<Scooter>();
-  @Output() saveSelected = new EventEmitter<Scooter>();
   scooter: Scooter;
   clicked = false;
-  private childParamsSubscription : Subscription;
 
-  constructor(private scootersService: ScootersService, private router: Router, private activatedRoute: ActivatedRoute) {
-    this.scooter = Scooter.copyConstructor(this._selectedScooterFromOverview);
-  }
-
-  ngOnInit(): void {
-    this.childParamsSubscription =
-      this.activatedRoute.params
-        .subscribe((params: Params) => {
-
-          this._selectedScooterFromOverview = params['id'];
-        })
-  }
-
-  ngOnDestroy(): void{
-    this.childParamsSubscription && this.childParamsSubscription.unsubscribe();
-  }
-
-  onDelete(){
-    if(confirm("Are you sure you want to discard unsaved changes?")) {
-      this.deletedSelected.emit(this._selectedScooterFromOverview);
-    }
-  }
-
-  onSave(){
-    this.saveSelected.emit(this._selectedScooterFromOverview);
-  }
-
-  onClear(){
-    if(confirm("Are you sure you want to discard unsaved changes?")) {
-      if(this._selectedScooterFromOverview) {
-        Object.assign(this._selectedScooterFromOverview, {
-          tag: "",
-          status: "",
-          gpsLocation: "",
-          mileage: 0,
-          batteryCharge: 0
-        })
-      }
-    }
-  }
-
-  onCancel(){
-    if(confirm("Are you sure you want to discard unsaved changes?")) {
-      this.cancelSelected.emit(this._selectedScooterFromOverview);
-    }
-  }
-
-  onReset(){
-    if(confirm("Are you sure you want to discard unsaved changes?")) {
-      if (this._selectedScooterFromOverview) this._selectedScooterFromOverview = <Scooter>{...this.scootersService.findById(this._selectedScooterFromOverview.id)}
-    }
-  }
-
+  @Input()
   set selectedScooterFromOverview(scooter: Scooter) {
-    this._selectedScooterFromOverview = scooter;
+    this.scooter = Scooter.copyConstructor(scooter);
   }
 
   get selectedScooterFromOverview(): Scooter {
     return this._selectedScooterFromOverview;
+  }
+
+  @Output() selectedScooterFromOverviewChange = new EventEmitter<Scooter>();
+  private _selectedScooterFromOverview: Scooter;
+
+  @Output() unselectedEvent = new EventEmitter<Scooter>();
+
+  constructor(private scootersService: ScootersService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onChanges() {
+    return this.scooter.equalsTo(this.scootersService.findById(this.scooter.id));
+  }
+
+  onConfirm() {
+    return !this.onChanges ? confirm("Are you sure you want to discard unsaved changes?") : true;
+  }
+
+  onDelete() {
+    if (this.onConfirm()) {
+      this.scootersService.deleteById(this.scooter.id);
+      this.unselectedEvent.emit(this.scooter);
+    }
+  }
+
+  onSave() {
+    this.scootersService.save(this.scooter);
+    this.unselectedEvent.emit(this.scooter);
+  }
+
+  onClear() {
+    if (this.onConfirm()) {
+      Object.assign(this.scooter, {
+        tag: "",
+        status: "",
+        gpsLocation: "",
+        mileage: 0,
+        batteryCharge: 0
+      })
+    }
+  }
+
+  onCancel() {
+    if (this.onConfirm()) {
+      this.scooter = this.scootersService.findById(this.scooter.id);
+      this.unselectedEvent.emit(this.scooter);
+    }
+  }
+
+  onReset() {
+    if (this.onConfirm()) {
+      this.scooter = this.scootersService.findById(this.scooter.id);
+    }
   }
 }
--- src/app/components/scooters/overview34/overview34.component.ts
+++ src/app/components/scooters/overview34/overview34.component.ts
@@ -11,67 +11,44 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 })
 
 export class Overview34Component implements OnInit {
-
   selectedScooter: Scooter = <Scooter>{};
 
   constructor(private scootersService: ScootersService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.selectedScooter.id = this.activatedRoute.snapshot.params['id'];
-    this.activatedRoute.params
-      .subscribe(
-        (params: Params) => {
-          this.selectedScooter.id = params['id'];
-        }
-      );
-    console.log(this.selectedScooter.id)
+    ngOnInit() {
+      this.selectedScooter.id = this.activatedRoute.snapshot.params['id'];
+      this.activatedRoute.params
+        .subscribe(
+          (params: Params) => {
+            this.selectedScooter.id = params['id'];
+          }
+        );
+      console.log(this.selectedScooter.id)
 
-    this.activatedRoute
-      .firstChild?.params
-      .subscribe((params: Params) => {
-        this.selectedScooter =
-          this.scooters.find(c => c.id == params.id)
-      });
-  }
-
-  get scooters(): Scooter[]{
-    return this.scootersService.findAll();
-  }
-
-  onSelect(scooter: Scooter){
-    if (scooter != null && scooter.id != this.selectedScooter?.id){
-      this.router.navigate([scooter.id], {relativeTo: this.activatedRoute})
-        .catch(reason => console.error(reason));
-    } else {
-      this.router.navigate(['../overview34'], {relativeTo: this.activatedRoute})
-        .catch(reason => console.error(reason));
+      this.activatedRoute
+        .firstChild?.params
+        .subscribe((params: Params) => {
+          this.selectedScooter =
+            this.scooters.find(c => c.id == params.id)
+        });
     }
 
-    //If the same scooter is clicked twice, unselect current scooter by emptying the list
-    if(scooter.id === this.selectedScooter.id){
+    get scooters(): Scooter[]{
+      return this.scootersService.findAll();
+    }
+
+    onAddScooter() {
+      let newScooter = Scooter.createSampleScooter();
+      this.scootersService.save(newScooter);
+      this.onSelect(newScooter);
+    }
+
+    onSelect(scooter: Scooter): void {
+      if(scooter.id === this.selectedScooter.id){
       this.selectedScooter = <Scooter>{};
     } else {
       this.selectedScooter = scooter;
     }
   }
 
-  onAddScooter() {
-    let newScooter = Scooter.createSampleScooter();
-    this.scootersService.save(newScooter);
-    this.onSelect(newScooter);
-  }
-
-  onDelete(selectedScooter: Scooter) {
-    this.scootersService.deleteById(selectedScooter.id);
-    this.selectedScooter = <Scooter>{};
-  }
-
-  onSave(scooter: Scooter){
-    this.scootersService.save(scooter);
-    this.selectedScooter = <Scooter>{};
-  }
-
-  onCancel(){
-    this.selectedScooter = <Scooter>{};
-  }
 }
