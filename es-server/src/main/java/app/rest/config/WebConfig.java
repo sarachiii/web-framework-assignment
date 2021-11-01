6c5e4b2a19213d7bef8baab90d5d9686b1fc1954
Nazlıcan Eren <nazlican.eren@hva.nl>
2021-11-01 20:48:22 +0100

    fixed cors?

--- es-server/src/main/java/app/rest/config/WebConfig.java
+++ es-server/src/main/java/app/rest/config/WebConfig.java
@@ -1,24 +1,22 @@
 package app.rest.config;
 
-import app.rest.ScootersController;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConditionalOnClass(ScootersController.class)
+//@ConditionalOnClass(ScootersController.class)
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
-  public void addCorsMappings(CorsRegistry registry){
-    registry.addMapping("/**")
-      .allowedOriginPatterns("http://localhost:4200*", getHostIPAddressPattern());
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOriginPatterns("http://localhost:*", getHostIPAddressPattern());
   }
 
   private String getHostIPAddressPattern() {
-    return null;
+    return "8085";
   }
+
 }
